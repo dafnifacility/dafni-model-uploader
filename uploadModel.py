f4fead9f0b84ae036ab17e256c5706501b05400c
unknown <tom.gowland@stfc.ac.uk>
2021-03-17 16:06:00 +0000

    refactor: add extra logging and use new CLI

--- uploadModel.py
+++ uploadModel.py
@@ -8,9 +8,10 @@ from dafni_cli.nims import (
     validate_model_definition,
     get_model_upload_urls,
     upload_file_to_minio,
-    start_model_ingest,
-    start_model_version_ingest,
+    ingest_model,
+    NIMSError,
 )
+from requests.exceptions import ConnectionError
 
 args = sys.argv[1:]
 definition_path = Path(args[0])
@@ -23,24 +24,32 @@ parent_model = args[5] if len(args) > 5 else None
 print("Logging in to DAFNI")
 jwt = login(username, password)
 
-print("Validate Model definition")
-valid, errors = validate_model_definition(jwt, definition_path)
-if not valid:
-    print("Definition validation failed with the following errors:", errors)
-    exit
+if jwt is None:
+    sys.exit(1)
 
-print("Get Urls")
-upload_id, urls = get_model_upload_urls(jwt)
-definition_url = urls["definition"]
-image_url = urls["image"]
+try:
+    print("Validating Model definition")
+    valid, errors = validate_model_definition(jwt, definition_path)
+    if not valid:
+        print("Definition validation failed with the following errors:", errors)
+        sys.exit(1)
 
-print("Upload Model definition")
-upload_file_to_minio(jwt, definition_url, definition_path)
-print("Upload image")
-upload_file_to_minio(jwt, image_url, image_path)
+    print("Get URLs for upload")
+    upload_id, urls = get_model_upload_urls(jwt)
+    definition_url = urls["definition"]
+    image_url = urls["image"]
 
-print("Start Model ingest")
-if not parent_model:
-    start_model_ingest(jwt, upload_id, version_message)
-else:
-    start_model_version_ingest(jwt, parent_model, upload_id, version_message)
+    print("Upload Model definition")
+    upload_file_to_minio(jwt, definition_url, definition_path)
+    print("Upload image")
+    upload_file_to_minio(jwt, image_url, image_path)
+
+    print("Start Model ingest")
+    ingest_model(jwt, upload_id, version_message, parent_model)
+
+except NIMSError as e:
+    print(e.message)
+    sys.exit(1)
+except ConnectionError as e:
+    print(f"Exception thrown during previous request: {e}")
+    sys.exit(1)
