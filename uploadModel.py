645181dc57a98098aeffbfde7e1f2c77c678804a
Sarah <69518420+sarahbyrnie@users.noreply.github.com>
2024-12-20 11:14:03 +0000

    feat: Update to use new CLI and work for new urls

--- uploadModel.py
+++ uploadModel.py
@@ -1,17 +1,9 @@
-import os
 import sys
-import requests
-import argparse
 from pathlib import Path
-from dafni_cli.login import login
-from dafni_cli.nims import (
-    validate_model_definition,
-    get_model_upload_urls,
-    upload_file_to_minio,
-    ingest_model,
-    NIMSError,
-)
-from requests.exceptions import ConnectionError
+
+from dafni_cli.api.session import DAFNISession
+from dafni_cli.models.upload import upload_model
+
 
 args = sys.argv[1:]
 definition_path = Path(args[0])
@@ -22,34 +14,14 @@ version_message = args[4]
 parent_model = args[5] if len(args) > 5 else None
 
 print("Logging in to DAFNI")
-jwt = login(username, password)
+session = DAFNISession().login(username, password)
 
-if jwt is None:
+if session is None:
     sys.exit(1)
 
 try:
-    print("Validating Model definition")
-    valid, errors = validate_model_definition(jwt, definition_path)
-    if not valid:
-        print("Definition validation failed with the following errors:", errors)
-        sys.exit(1)
-
-    print("Get URLs for upload")
-    upload_id, urls = get_model_upload_urls(jwt)
-    definition_url = urls["definition"]
-    image_url = urls["image"]
-
-    print("Upload Model definition")
-    upload_file_to_minio(jwt, definition_url, definition_path)
-    print("Upload image")
-    upload_file_to_minio(jwt, image_url, image_path)
-
-    print("Start Model ingest")
-    ingest_model(jwt, upload_id, version_message, parent_model)
-
-except NIMSError as e:
-    print(e.message)
-    sys.exit(1)
-except ConnectionError as e:
-    print(f"Exception thrown during previous request: {e}")
-    sys.exit(1)
+    upload_model(session, definition_path, image_path, version_message, parent_model)
+except:
+    pass
+finally:
+    session.logout()
